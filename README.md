c2d434ad732bc73b83b6e114277e2b36ff70ef65
Rohit Sahu <f20202380@hyderabad.bits-pilani.ac.in>
2024-07-11 12:11:33 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,69 @@
+
+# Rock Paper Scissors Game
+
+## Description
+
+This is a simple Rock Paper Scissors game built using HTML, CSS, and JavaScript. The user can play against the computer, and the score is tracked for both the user and the computer.
+
+## Features
+
+- User can choose Rock, Paper, or Scissors.
+- The computer randomly selects Rock, Paper, or Scissors.
+- The game declares a winner for each round and updates the score.
+- The game can be reset at any time.
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+
+## Getting Started
+
+### Prerequisites
+
+To run this project, you need a web browser.
+
+### Installation
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/RohitS511/Rock-Paper-Scissor-Game.git
+   ```
+
+2. Navigate to the project directory:
+
+   ```bash
+   cd rock-paper-scissors
+   ```
+
+### Usage
+
+1. Open the `index.html` file in your web browser.
+2. Enter your name when prompted.
+3. Click on one of the choices (Rock, Paper, or Scissors) to play the game.
+4. The result of the game will be displayed, and the scores will be updated accordingly.
+5. Click the "Reset Game" button to reset the scores and start a new game.
+
+## Project Structure
+
+- `index.html`: The main HTML file that contains the structure of the game.
+- `style.css`: The CSS file for styling the game.
+- `app.js`: The JavaScript file containing the game logic.
+
+## Screenshots
+
+![Game Screenshot](images/Screenshot.png)
+
+## Contributing
+
+Contributions are welcome! Please open an issue or submit a pull request for any improvements or bug fixes.
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for details.
+
+## Acknowledgements
+
+- Inspired by the classic Rock Paper Scissors game.
---
+++ app.js
@@ -0,0 +1,80 @@
+let compScore = 0;
+let userScore = 0;
+
+let choices = document.querySelectorAll(".choice");
+let msg = document.querySelector("#msg");
+let userScorePara = document.querySelector("#userScore");
+let compScorePara = document.querySelector("#compScore");
+let reset = document.querySelector("#reset");
+let user = document.querySelector("#user");
+let userName = document.querySelector(".name");
+let ok = document.querySelector("#ok");
+
+const genComp = () => {
+    const options = ["rock","paper","scissor"];
+    //Generate random function
+    const inx = Math.floor(Math.random()*3);
+    return options[inx];
+}
+
+// let userName = prompt("Enter your name:");
+// user.innerText = userName;
+// ok.addEventListener("click", () => {
+//     userName.classList.remove(userName);
+// })
+
+const showWinner = (userWin, userChoice, compChoice) => {
+    if (userWin) {
+        console.log("you win");
+        userScore++;
+        userScorePara.innerText = userScore;
+        msg.innerText = `You win! Your ${userChoice.toUpperCase()} beats ${compChoice.toUpperCase()}`;
+        msg.style.backgroundColor = "green";
+    } else {
+        console.log("you lose");
+        compScore++;
+        compScorePara.innerText = compScore;
+        console.log(compChoice);
+        msg.innerText = `You lost. ${compChoice.toUpperCase()} beats your ${userChoice.toUpperCase()}`;
+        msg.style.backgroundColor = "red";
+    }
+}
+
+const playGame = (userChoice) => {
+    console.log("your choice",userChoice);
+    const compChoice = genComp();
+    console.log("comp choice",compChoice);
+
+    if(userChoice == compChoice){
+        console.log("game was draw");
+        msg.innerText = "Game was draw";
+        msg.style.backgroundColor = "rgb(8, 8, 57)";
+    }else{
+        let userWin = true;
+        if(userChoice === "rock"){
+            userWin = compChoice === "paper" ? false : true;
+        } else if(userChoice === "paper"){
+            userWin = compChoice === "scissor" ? false : true;
+        } else {
+            userWin = compChoice === "rock" ? false : true;
+        }
+        showWinner(userWin, userChoice, compChoice);
+    }
+      
+}
+
+choices.forEach((choice) => {
+    choice.addEventListener("click", () => {
+        const userChoice = choice.getAttribute("id");
+        playGame(userChoice);
+    })
+})
+
+reset.addEventListener("click", () => {
+    compScorePara.innerText = "0";
+    userScorePara.innerText = "0";
+    userScore = 0;
+    compScore = 0;
+    msg.innerText = "Play Your Move";
+    msg.style.backgroundColor = "rgb(8, 8, 57)";
+})
